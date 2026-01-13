8d5c92f1d9c2f29e737c7ff339077028203bb194
XhuyZ <xhuyzdev@gmail.com>
2026-01-13 14:31:52 +0700

    small change

---
+++ ANC.Application/DI/DependencyInjection.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ANC.Application.DI
+{
+	public static class DependencyInjection
+	{
+		public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddRepository();
+			services.AddAutoMapper();
+			services.AddServices(configuration);
+			services.AddMemoryCache();
+		}
+
+		public static void AddRepository(this IServiceCollection services)
+		{
+			// services.AddScoped<Unit, UnitOfWork>();
+		}
+
+		private static void AddAutoMapper(this IServiceCollection services)
+		{
+			// services.AddAutoMapper(cfg => { cfg.AddMaps(Assembly.GetExecutingAssembly()); });
+		}
+
+		public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+		{
+			// services.AddScoped<IPolicyService, PolicyService>();
+			// services.AddScoped<IPolicyTypeService, PolicyTypeService>();
+			// services.AddScoped<IComissionService, ComissionService>();
+		}
+	}
+}
+
