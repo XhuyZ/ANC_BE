e77407a896c02c5b2186ebb834dd9076477060b0
XhuyZ <xhuyzdev@gmail.com>
2026-01-13 15:33:18 +0700

    yodate

--- ANC.Domain/Bases/BaseEntity.cs
+++ ANC.Domain/Bases/BaseEntity.cs
@@ -5,11 +5,10 @@ public class BaseEntity
 
 	public BaseEntity()
 	{
-		Id = Guid.NewGuid().ToString("N");
 		CreatedTime = LastUpdatedTime = DateTime.UtcNow;
 	}
 
-	public string Id { get; set; }
+	public Guid Id { get; set; }
 	public string? CreatedBy { get; set; }
 	public string? LastUpdatedBy { get; set; }
 	public string? DeletedBy { get; set; }
